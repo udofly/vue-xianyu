36ffa62370fe091ff7c4fb58a6595bd54dc21bc1
gaochunfa <1057538126@qq.com>
2025-02-15 17:40:37 +0800

    Initial commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="home-content">首页内容,正常展示则正确</div>
+</template>
+
+<style>
+.home-content {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 50px;
+}
+</style>
