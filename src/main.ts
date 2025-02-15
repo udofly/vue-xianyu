7b6445a282853dbe488a4da1cead55b8a00405c7
gaochunfa <1057538126@qq.com>
2025-02-15 18:36:51 +0800

    路由日志2

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VConsole from 'vconsole';
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+
+const vConsole = new VConsole();
+app.use(vConsole);
 app.use(createPinia())
 app.use(router)
 
